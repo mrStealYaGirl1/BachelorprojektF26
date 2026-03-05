9f231c6c967dbbf989b8ab5a20264bbbcab2687e
mrStealYaGirl2 <133003670+mrStealYaGirl2@users.noreply.github.com>
2026-03-05 14:01:47 +0100

    develop

--- hardware/ESP32_IMU_setup_NEW/components/ble_manager/ble_manager.h
+++ hardware/ESP32_IMU_setup_NEW/components/ble_manager/ble_manager.h
@@ -1,16 +1,7 @@
-#define SAMPLES_PER_PACKET 10
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
 
-typedef struct
-{
-    float ax;
-    float ay;
-    float az;
-
-    float gx;
-    float gy;
-    float gz;
-
-    uint32_t ts_ms;
 #define SAMPLES_PER_PACKET 10
 
 typedef struct
@@ -36,13 +27,14 @@ typedef struct
 
 } ble_imu_pkt_t;
 
-} imu_sample_pkt_t;
+void ble_manager_init(void);
 
-typedef struct
-{
-    imu_sample_pkt_t sample[SAMPLES_PER_PACKET];
+// Enkel tx task + queue til at sende simple beskeder fra swing_manager (eller andre steder)
+void ble_manager_start_tx_task(void);
+bool ble_manager_send_simple(uint32_t counter, uint32_t timestamp_ms);
+bool ble_manager_notify_simple(uint32_t counter, uint32_t timestamp_ms);
 
-    uint16_t seq;
-    uint16_t event_id;
-
-} ble_imu_pkt_t;
+// Fremtidige API'er til at sende IMU data (i stedet for simple beskeder)
+bool ble_manager_notify_imu(const ble_imu_pkt_t *pkt);
+void ble_manager_start_imu_tx_task(void);
+bool ble_manager_send_imu(const ble_imu_pkt_t *pkt);
